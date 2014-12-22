332e36166b5e5f9e7e7d607648d33f8639732172
numipinto <numipinto@gmail.com>
2014-12-22 01:10:19 +0000

    [DataSet] (Document) now with mappings

--- src/pt/tecnico/rgi/Document.java
+++ src/pt/tecnico/rgi/Document.java
@@ -4,39 +4,39 @@ import java.util.Map;
 
 public class Document {
 
-	private String className;
-	private Map<String, Integer> features_tf;
-	private Map<String, Integer> features_idf;
+	private Integer classType;
+	private Map<Integer, Integer> features_tf;
+	private Map<Integer, Integer> features_idf;
 	
 	
-	public Document(String path){ // sci.med a class é sci
-		className = path.split(".")[0];
-		features_tf = new Map<String, Integer>();
-		features_idf = new Map<String, Integer>();
+	public Document(Integer classType){ // sci.med a class é sci
+		this.classType = classType;
+		features_tf = new Map<Integer, Integer>();
+		features_idf = new Map<Integer, Integer>();
 	}
 	
 	// Adicionar Features com o TF do documento
-	public void addNewFeature(String feature, Integer occurrency){
+	public void addNewFeature(Integer feature, Integer occurrency){
 		features_tf.put(feature, occurrency);
 	}
 	//Aceder a uma feature em específico
-	public Integer getFeatureTF(String feature){
+	public Integer getFeatureTF(Integer feature){
 		return features_tf.get(feature);
 	}
 	//Aceder a todas as TF das features
-	public Map<String, Integer> getFeaturesTF(){
+	public Map<Integer, Integer> getFeaturesTF(){
 		return features_tf;
 	}
 	//Acrescentar IDF de uma Feature especifica
-	public void addIDFFeature(String feature, Integer occurrency){
+	public void addIDFFeature(Integer feature, Integer occurrency){
 		features_idf.put(feature, occurrency);
 	}
 	//Aceder ao IDF de uma feature
-	public Integer getFeatureIDF(String feature){
+	public Integer getFeatureIDF(Integer feature){
 		return features_idf.get(feature);
 	}
 	//Aceder a todos os IDF's de todas as features
-	public Map<String, Integer> getFeaturesIDF(){
+	public Map<Integer, Integer> getFeaturesIDF(){
 		return features_idf;
 	}
 }
